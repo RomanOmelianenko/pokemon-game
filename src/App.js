d7d4431ece474bd89f1a4a316be449d51b87c6b3
RomanOmelianenko <r_omelyanenko@ukr.net>
2022-01-29 18:31:37 +0200

    hw5-1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom/cjs/react-router-dom.min";
+
 import HomePage from "./routes/Home/home";
 import GamePage from "./routes/Game/game";
 
 const App = () => {
-  const [page, setPage] = useState('app');
-
-  const handleChangePage = (page) => {
-    // console.log('####: <App />');
-
-    setPage(page);
-  }
-
-    switch (page) {
-      case 'app':
-        return <HomePage onChangePage={handleChangePage} />
-
-      case 'game': 
-        return <GamePage onChangePage={handleChangePage}/>
-
-      default:
-        return <HomePage />
-    }
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path='/' exact component={HomePage} />
+        <Route path='/home' component={HomePage} />
+        <Route path='/game' component={GamePage} />
+        <Route path='/about' render={() => (
+          <h1>This is page About</h1>
+        )} />
+        <Route path='/contact' render={() => (
+          <h1>This is page Contact</h1>
+        )} />
+        <Route render={() => (
+          <h1>404 Not Found</h1>
+        )} />
+      </Switch>
+    </BrowserRouter>
+  )
 };
 
 export default App;
